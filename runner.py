0c58647301d7a287ff5d8a2f4535bb66b4d8fc5c
Peter Hurford <peter@peterhurford.com>
2016-09-21 10:56:29 -0500

    Delete runner.py

--- runner.py
+++
@@ -1,7 +0,0 @@
-import csv
-import json
-
-products = json.loads(open('~/Downloads/2016-09-16-14-32-34/part-00000', 'r'))
-import pdb
-pdb.set_trace()
-csv_file = csv.writer(open('products.csv', 'wb+'))
