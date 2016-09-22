d4cfd0f9a7ebc484256efc1e8b4983f638bb853b
peterhurford <peter@peterhurford.com>
2016-09-22 10:26:40 -0500

    Explain random recs

--- random/random.sh
+++ random/random.sh
@@ -1,3 +1,9 @@
+# Create product file
 seq 1 50 > products.dat
+
+# Create customer file
 seq -f "%.0f" 1 10000000 > customers.dat
-gshuf -r -n 10000000 products.dat | paste -d " " customers.dat /dev/stdin > recs.dat  #11s
+
+# Randomly sample products and paste them with customers
+# to get shuf (called gshuf) on mac, `brew install coreutils`. On Mac, `shuf` is called `gshuf`.
+shuf -r -n 10000000 products.dat | paste -d " " customers.dat /dev/stdin > recs.dat  #11s
