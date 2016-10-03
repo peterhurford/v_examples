bf11a3178794f9af83bc5539b6a3b790e6faad0c
peterhurford <peter@peterhurford.com>
2016-10-03 03:19:27 -0500

    Three attempts at 1M run; another try at demographics

---
+++ als/1m_run_third_attempt.sh
@@ -0,0 +1,47 @@
+# Convert ratings from movielens format to VW format (3s)
+awk -F"::" '{printf "%d |u %d |i %d\n", $3, $1, $2}' < ratings.dat > ratings_t.dat
+
+# Convert movie data from movielens format to VW format (0s)
+awk -F"::" '{printf "|i %d\n", $1}' < movies.dat > movies_t.dat
+
+# Create a user dataset for VW from the unique customer ids in the ratings dataset (0s)
+awk '{print $3}' < ratings_t.dat | uniq | awk '{printf "|u %d\n", $1}' > users_t.dat
+
+# Train a VW ALS model on the data (30s)
+vw -d ratings_t.dat -b 18 -q ui --rank 10 --l2 0.001 --learning_rate 0.015 --passes 5 --decay_learning_rate 0.97 --power_t 0 -f movielens.reg --cache_file movielens.cache
+
+# Create a user-product grid for all the possible user-product combinations (18s)
+awk 'FNR == NR { a[++n]=$0; next } { for(i=1; i<=n; i++) print $0, a[i] }' movies_t.dat users_t.dat > tmp_b
+
+# Remove the rating to get raw user-product combinations present in the ratings (2s)
+awk '{printf "%s %d %s %d\n", $2, $3, $4, $5}' < ratings_t.dat > tmp_a
+
+# Filter out the existing user-product combinations to only predict on novel combinations (40s)
+awk 'NR == FNR { list[$0]=1; next } { if (! list[$0]) print }' tmp_a tmp_b > matrix_t.dat
+
+# Split grid into eight sub-grids (0s)
+gsplit -d --number 8 matrix_t.dat matrix_t_
+
+generate_matrix() {
+  vw -d "matrix_t_0$1" -i movielens.reg -t -p "predictions$1.txt"         # Predict
+  paste -d " " "predictions$1.txt" "matrix_t_0$1" > "predictions_$1.dat"  # Add labels
+  rm "matrix_t_0$1" "predictions$1.txt"                                   # Remove files
+  echo "Finished matrix $1 on `date`"
+}
+date; for i in `seq 0 7`; do generate_matrix $i &
+done
+# 1m2s
+
+generate_recs() {
+  sort -nr -k3 -k1 "predictions_$1.dat" | awk '{ if(!($3 in seen)) { seen[$3] = 1; for (i=0; i<10; i++) { print; getline } } }' > "recs_$1.dat"
+  echo "Finished recs $1 on `date`"
+}
+date; for i in `seq 0 7`; do generate_recs $i &
+done
+# 12s
+
+cat recs* > all_recs.dat # 0s
+wc -l all_recs.dat
+
+# TOTAL: 2m47s
+# Done on my laptop (16G RAM 8 core Macbook Pro Mid-2015), so no cost data.
--- als/demographics.sh
+++ als/demographics.sh
@@ -15,9 +15,15 @@ head -n 80167 ratings_t.dat | gshuf > train.dat
 tail -n 20042 ratings_t.dat > test.dat
 head -n 80167 ratings_tt.dat | gshuf > train_d.dat
 tail -n 20042 ratings_tt.dat > test_d.dat
+sed "s/|g //" train_d.dat > train_d2.dat  # Include gender as part of the item matrix.
+sed "s/|g //" test_d.dat > test_d2.dat
 
 # Train a VW ALS model on the train data
 vw -d train.dat -b 24 -q ui --rank 10 --l2 0.001 --learning_rate 0.015 --passes 20 --decay_learning_rate 0.97 --power_t 0 -f movielens.reg --cache_file movielens.cache; vw test.dat -i movielens.reg -t -p predictions.txt
 # Average Loss: 0.985515
 vw -d train_d.dat -b 24 -q ui -q gi --rank 10 --l2 0.001 --learning_rate 0.015 --passes 20 --decay_learning_rate 0.97 --power_t 0 -f movielens_d.reg --cache_file movielens_d.cache; vw test_d.dat -i movielens_d.reg -t -p predictions_d.txt
 # Average Loss: 1.015805
+vw -d train_d.dat -b 24 -q ui -q gi -q ug --rank 10 --l2 0.001 --learning_rate 0.015 --passes 20 --decay_learning_rate 0.97 --power_t 0 -f movielens_d3.reg --cache_file movielens_d.cache; vw test_d.dat -i movielens_d3.reg -t -p predictions_d3.txt
+# Average Loss: 1.020143
+vw -d train_d2.dat -b 24 -q ui --rank 10 --l2 0.001 --learning_rate 0.015 --passes 20 --decay_learning_rate 0.97 --power_t 0 -f movielens_d2.reg --cache_file movielens_d2.cache; vw test_d2.dat -i movielens_d2.reg -t -p predictions_d2.txt
+# average loss = 1.003438
