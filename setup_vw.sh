288fb59cca0fbe07eb56f91d2cfefeee918a50ff
peterhurford <peter@peterhurford.com>
2017-02-08 00:19:18 -0600

    Update VW installation

--- setup_vw.sh
+++ setup_vw.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 cd ~
 sudo apt-get update
-sudo apt-get -y install build-essential
-sudo apt-get -y install clang-3.5 llvm
+sudo apt-get -y install build-essential clang-3.5 llvm
 sudo ln -s /usr/bin/clang-3.5 /usr/bin/clang; sudo ln -s /usr/bin/clang++-3.5 /usr/bin/clang++
-sudo apt-get -y install libffi-dev libssl-dev libxml2-dev libxslt1-dev libjpeg8-dev zlib1g-dev
-sudo apt-get -y install unzip htop iotop bmon parallel
-sudo apt-get -y install python-pip python-dev
+sudo apt-get -y install libffi-dev libssl-dev libxml2-dev libxslt1-dev libjpeg8-dev zlib1g-dev python-pip
 
 sudo apt-get -y install libboost-all-dev
 git clone https://github.com/JohnLangford/vowpal_wabbit.git && cd vowpal_wabbit
-make && sudo make install
-cd cluster && sudo make install && cd ../..
+make && sudo make install && cd ..
 
-git clone https://github.com/peterhurford/vowpal_platypus.git && cd vowpal_platypus && git checkout 1.0.2
+git clone https://github.com/peterhurford/vowpal_platypus.git && cd vowpal_platypus
 sudo python setup.py install && cd ..
 sudo -H pip install retrying
+sudo -H pip install git+https://github.com/uqfoundation/dill.git@master
+sudo -H pip install git+https://github.com/uqfoundation/pathos.git@master
 
 cd ~/vp_examples/criteo
 mkdir data; cd data
