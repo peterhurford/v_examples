c9ee626bdadb01f55135f84b2ecd0e4729f9af11
peterhurford <peter@peterhurford.com>
2017-03-01 11:21:29 -0600

    try rotating the port again because desparation

--- als/20m/vp/runner.py
+++ als/20m/vp/runner.py
@@ -89,7 +89,7 @@ def predict(model):
                 curr_done = done
             unseen_movie_ids = list(set(movie_ids) - set(ratings[user_id].values()))
             vw_items = map(lambda m: {'u': user_id, 'i': m}, unseen_movie_ids)
-            preds = daemon_predict(8040, vw_items, quiet=True)
+            preds = daemon_predict(20140, vw_items, quiet=True)
             user_recs = [list(a) for a in zip(preds, unseen_movie_ids)]
             user_recs.sort(reverse=True)
             rfile.write(str({'user': user_id,
@@ -97,7 +97,7 @@ def predict(model):
     return None
 
 run_parallel(model, train)
-daemon(model[0], port=8040, num_children=cores)
+daemon(model[0], port=20140, num_children=cores)
 run_parallel(model, predict)
 end = datetime.now()
 print('Time: ' + str((end - start).total_seconds()) + ' sec')
