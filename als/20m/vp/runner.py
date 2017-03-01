1addc5c35c46fd41af4e45ba18a1edd2ae57f7e8
peterhurford <peter@peterhurford.com>
2017-03-01 11:04:39 -0600

    fix quiet

--- als/20m/vp/runner.py
+++ als/20m/vp/runner.py
@@ -89,7 +89,7 @@ def predict(model):
                 curr_done = done
             unseen_movie_ids = list(set(movie_ids) - set(ratings[user_id].values()))
             vw_items = map(lambda m: {'u': user_id, 'i': m}, unseen_movie_ids)
-            preds = daemon_predict(4040, vw_items)
+            preds = daemon_predict(4040, vw_items, quiet=True)
             user_recs = [list(a) for a in zip(preds, unseen_movie_ids)]
             user_recs.sort(reverse=True)
             rfile.write(str({'user': user_id,
@@ -97,7 +97,7 @@ def predict(model):
     return None
 
 run_parallel(model, train)
-daemon(model[0], port=4040, num_children=cores, quiet=True)
+daemon(model[0], port=4040, num_children=cores)
 run_parallel(model, predict)
 end = datetime.now()
 print('Time: ' + str((end - start).total_seconds()) + ' sec')
